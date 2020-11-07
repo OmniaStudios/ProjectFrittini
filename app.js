9cb0569f8d93f018130011280aec074f1e1af064
Lorenzo De Blasio <gioco654@gmail.com>
2020-11-07 12:14:11 +0100

    Happy Birthday, Nacca!

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-/* Importazione pacchetti necessari
+/* Importazione pacchetti necessari 
     - se non dovessero essere presenti, lanciare 'npm i --save' per caricarli da package.json */
 const express = require('express');
+const twilio = require('twilio');
 
 /* Definizione app */
 const app = express();
-
+ //TODO Non finire in carcere
 /* Importazione dei router */
 const routerBasic = require('./routes/routerBasic');
 
 app.use(express.static(__dirname + '/public'));
+
+var accountSid = 'AC73d390faa0d03ab6ae7f14acc0ae7429';
+var authToken = '35067d97bbd267334e236d340cacbf42';
+
+var client = new twilio(accountSid, authToken);
+
+console.log("EHI FRA!");
+client.messages.create({
+    body: 'Buongiornissimo!',
+    to: '+393450420203',
+    from: '+19257018054'
+}).then((message) => console.log(message.sid));
+console.log("Dimmi FRA!");
 
 /* Impostazione del motore di rendering:
     - non è quindi necessario specificare l'estensione dei file nel 'res.render('nomefile')' */
