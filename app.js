aeec54d1a6b991c39687285eb7a8a36bd7c3d1eb
mattiadevigus <mattycarly02@gmail.com>
2020-11-07 17:47:17 +0100

    Added server system & public folder

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const twilio = require('twilio');
 
+
+
 /* Definizione app */
 const app = express();
  //TODO Non finire in carcere
@@ -13,8 +15,8 @@ app.use(express.static(__dirname + '/public'));
 
 var accountSid = 'AC73d390faa0d03ab6ae7f14acc0ae7429';
 var authToken = '35067d97bbd267334e236d340cacbf42';
-
 var client = new twilio(accountSid, authToken);
+
 
 console.log("EHI FRA!");
 client.messages.create({
@@ -33,3 +35,5 @@ app.use('/', routerBasic);
 
 /* Esportazione modulo app per l'utilizzo in server.js */
 module.exports = app;
+
+app.listen(3000);
