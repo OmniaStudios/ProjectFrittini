9b18ce8faf471bcacb5009ad6ff3233d8d18cfd7
mattiadevigus <mattycarly02@gmail.com>
2020-11-21 17:21:00 +0100

    Created first section and background

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,15 @@ function changeColorNavbar(){
     document.getElementById("navContainer").style.transition = "all 0.5s";
     let colorBefore = document.getElementById("navContainer").style.background
     if(colorBefore == ''){
-        colorBefore = "rgb(245, 245, 245)" 
+        colorBefore = "rgb(251, 252, 252)" 
     }
-    const colorAfter = "rgb(21, 21, 30)"
-    const colorDefault = "rgb(245, 245, 245)" 
+    const colorAfter = "rgb(234, 236, 238)"
+    const colorDefault = "rgb(251, 252, 252)" 
     console.log(colorBefore)
     if(colorDefault != colorBefore)
     {
         document.getElementById("navContainer").style.background= colorDefault
-        document.getElementsByClassName("navbar-nav").style.background = colorDefault
     }else{
         document.getElementById("navContainer").style.background= colorAfter
-        document.getElementsByClassName("navbar-nav").style.background = colorDefault
     }
 }
