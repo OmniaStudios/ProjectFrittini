74ead2204f31931d99a1f80b518ae502d42da9d5
mattiadevigus <mattycarly02@gmail.com>
2020-12-22 21:30:29 +0100

    Fixed timer in js project

--- public/js/index.js
+++ public/js/index.js
@@ -2,29 +2,51 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
-function changeColorNavbar(){
+function changeColorNavbar() {
     document.getElementById("navContainer").style.transition = "all 0.5s";
     let colorBefore = document.getElementById("navContainer").style.background
-    if(colorBefore == ''){
-        colorBefore = "rgb(251, 252, 252)" 
+    if (colorBefore == '') {
+        colorBefore = "rgb(251, 252, 252)"
     }
     const colorAfter = "rgb(234, 236, 238)"
-    const colorDefault = "rgb(251, 252, 252)" 
+    const colorDefault = "rgb(251, 252, 252)"
     console.log(colorBefore)
-    if(colorDefault != colorBefore)
-    {
-        document.getElementById("navContainer").style.background= colorDefault
-    }else{
-        document.getElementById("navContainer").style.background= colorAfter
+    if (colorDefault != colorBefore) {
+        document.getElementById("navContainer").style.background = colorDefault
+    } else {
+        document.getElementById("navContainer").style.background = colorAfter
     }
 }
 
 window.addEventListener('load', (event) => {
+    let status = "Chiusi";
+    let color = "red";
     let orario = new Date();
     let hours = orario.getHours();
     let minutes = orario.getMinutes();
-    if(hours >= 10 || hours <= 15){
-        console.log("si");
+    let day = orario.getDay();
+
+    // gestione giorni normali
+    if (hours >= 10 && hours < 15 && day != 6) {
+        if (hours == 15 && minutes <= 30) {
+            status = "Aperti";
+            color = "Green";
+        }
     }
+
+    if (hours >= 17 && hours < 21 && day != 6) {
+        status = "Aperti";
+        color = "Green";
+    }
+
+    if (hours >= 10 && hours < 21 && day == 6) {
+        status = "Aperti";
+        color = "Green";
+    }
+
+    document.getElementById("status").style.color = color;
+    document.getElementById("status").innerText = "Siamo " + status;
+
+
 });
 
