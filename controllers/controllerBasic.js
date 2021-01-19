14140d534402b4dc1b0a9f3401312116e166ef75
Lorenzo De blasio <gioco654@gmail.com>
2021-01-19 19:08:03 +0100

    Added mongoDB

--- controllers/controllerBasic.js
+++ controllers/controllerBasic.js
@@ -3,3 +3,28 @@ exports.get_home = (req, res) => {
     /* Impostazione dello stato HTPP success e rendering della pagina home*/
     res.status(200).render('index');
 }
+
+exports.get_menu = (req, res) => {
+    /* Impostazione dello stato HTPP success e rendering della pagina menu*/
+    res.status(200).render('menu');
+}
+
+exports.get_menu_family = (req, res) => {
+    /* Impostazione dello stato HTPP success e rendering della pagina menufamily*/
+    res.status(200).render('menufamily');
+}
+
+exports.get_products = (req, res) => {
+    /* Impostazione dello stato HTPP success e rendering della pagina products*/
+    res.status(200).render('products');
+}
+
+exports.get_work = (req, res) => {
+    /* Impostazione dello stato HTPP success e rendering della pagina workwithus*/
+    res.status(200).render('workwithus');
+}
+
+exports.get_aboutus = (req, res) => {
+    /* Impostazione dello stato HTPP success e rendering della pagina workwithus*/
+    res.status(200).render('aboutus');
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const app = require('./app');
 
 const express = require('express');
+const mongoose = require('mongoose');
 
 /* Definizione porta di accesso per deploy e test in locale */
 const PORT = process.env.PORT || 5000;
@@ -9,5 +10,15 @@ app.listen(PORT, () => {
     console.log(`projectFrittini | Server started on port ${PORT} - use 'localhost;${PORT}`);
 });
 
+let db = require('./config/keys').MongoURI;
+mongoose.connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
+db.once('open', () => {
+    console.log('projectFrittini | MongoDB Connected');
+})
